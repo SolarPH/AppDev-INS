54a58e3cc8040d80167b9095ea7b6c2d3054eb9b
Micca Ross L. Angulo <118921372+MiccaRossAngulo@users.noreply.github.com>
2022-12-15 13:14:21 +0800

    Update MyCsharp.cs

--- MyCsharp.cs
+++ MyCsharp.cs
@@ -12,6 +12,6 @@ public class MyCsharp
 	public void printName(string name)
 	{
 		Console.WriteLine("Hello! My name is " + name);
-		Console.WriteLine("My favorite color is black");
+		Console.WriteLine("My favorite colors are red and black");
 	}
 }
