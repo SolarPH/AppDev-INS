be6d1c934852c1303b59e4de52918c10c1b483e6
Andrei Rafael Andal <120620065+Dreysuuuu@users.noreply.github.com>
2022-12-15 18:41:03 +0800

    Update MyCsharp.cs

--- MyCsharp.cs
+++ MyCsharp.cs
@@ -5,8 +5,9 @@ public class MyCsharp
 	public static void Main(string[] args)
 	{
 		// Insert code from line below this comment where it instantiates the MyCsharp object into variable named pin, then call the method named printName(String name);
-    // Expected Line 1, replace this comment with code on pull request
-    // Expected Line 2, replace this comment with code on pull request
+   		MyCsharp dreysu = new MyCsharp();
+		dreysu.printName("Andrei Rafael Andal");
+
 	}
 	
 	public void printName(string name)
