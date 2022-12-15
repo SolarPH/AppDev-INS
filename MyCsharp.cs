9d279481466fd753f913c1abe28a4a010d205566
AlexRoson <120585963+AlexRoson@users.noreply.github.com>
2022-12-15 12:16:05 +0800

    Update MyCsharp.cs

--- MyCsharp.cs
+++ MyCsharp.cs
@@ -6,7 +6,7 @@ public class MyCsharp
 	{
 		// Insert code from line below this comment where it instantiates the MyCsharp object into variable named pin, then call the method named printName(String name);
     MyCsharp pin = new MyCsharp();
-    pin.printName("Roson, Alex Christian Daniel M.")
+    pin.printName("Roson, Alex Christian Daniel M.");
 	}
 	
 	public void printName(string name)
