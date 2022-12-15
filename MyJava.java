26c9154d7807c6984a663794baaaf53d82510f7b
SPHLocal <120619703+SPHLocal@users.noreply.github.com>
2022-12-15 12:38:34 +0800

    Test Pull from SolarPH (Alt Account)

--- MyJava.java
+++ MyJava.java
@@ -5,6 +5,7 @@ public class MyJava
     // Insert code from line below this comment where it instantiates the MyJava object into variable named pin, then call the method named printName(String name);
     // Expected Line 1, replace this comment with code on pull request
     // Expected Line 2, replace this comment with code on pull request
+    This is a test pull
   }
   
   public void printName(String name)
