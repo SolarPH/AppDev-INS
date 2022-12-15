da8bd3304c777da2a3dd952230542b5d42855f8f
MikeBolanio <102496346+MikeBolanio@users.noreply.github.com>
2022-12-15 13:46:43 +0800

    Update MyCsharp.cs
    final na po sir

--- MyCsharp.cs
+++ MyCsharp.cs
@@ -1,19 +1,16 @@
 using System;
-	
-
+					
 public class MyCsharp
 {
 	public static void Main(string[] args)
 	{
-		Console.WriteLine("What's your name?");
-		String name = Console.ReadLine();
 		
-		Console.WriteLine("How old are you?");
-		int age = Convert.ToInt32(Console.ReadLine());
-		
-		Console.WriteLine("Hello " + name);
-		Console.WriteLine("You are " + age + " years old");
-		
-		Console.ReadKey();
+  	  MyCsharp pin = new MyCsharp();
+  	  pin.printName("Mike Bolanio");
+	}
+	
+	public void printName(string name)
+	{
+		Console.WriteLine("Hello! My name is " + name);
 	}
 }
