f21fd67599754d3545f2639aa4867f7b82a49392
NiiChannXD <120563465+CA-NiiChannXD@users.noreply.github.com>
2022-12-15 10:41:26 +0800

    Andal_Clarisse

--- MyCsharp.cs
+++ MyCsharp.cs
@@ -4,7 +4,7 @@ public class MyCsharp
 {
 	public static void Main(string[] args)
 	{
-		// Insert code from line below this comment where it instantiates the MyCsharp object into variable named pin, then call the method named printName(String name);
+		// Insert code from line below this comment where it instantiates the MyCsharp object into variable named pin,then call the method named printName(String name);
     // Expected Line 1, replace this comment with code on pull request
     // Expected Line 2, replace this comment with code on pull request
 	}
