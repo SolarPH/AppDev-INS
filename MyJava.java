58d59d3bbfa210558c6f3b8ffe11ef961811338d
NiiChannXD <120563465+CA-NiiChannXD@users.noreply.github.com>
2022-12-15 18:24:52 +0800

    Andal,Clarisse

--- MyJava.java
+++ MyJava.java
@@ -1,14 +1,14 @@
 public class MyJava {
-  public static void printName(String name, int age, String course)
-  {
-    System.out.println("Hello! My name is " + name + "!" + "I am" + age + "years old" + "I am partaking " + course + "as my course");
-  }
-
   public static void main(String[] args) {
-    String name = "Andal, Clarisse";
+   String name = "Andal, Clarisse";
    String course = "BSIT";
    int age = 21;
 
- printName(name, age, course);
+   printName(name, age, course);
+    
+    public static void printName(String name, int age, String course)
+  {
+    System.out.println("Hello! My name is " + name + "!" + "I am" + age + "years old" + "I am partaking " + course + "as my course");
+  }
   }
 }
