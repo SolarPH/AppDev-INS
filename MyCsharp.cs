977f975db6c5aa51d145d95be6cdcf2d5bcc5f98
NiiChannXD <120563465+CA-NiiChannXD@users.noreply.github.com>
2022-12-15 13:53:26 +0800

    Update MyCsharp.cs

--- MyCsharp.cs
+++ MyCsharp.cs
@@ -5,7 +5,7 @@ public class MyCsharp
 	public static void Main(string[] args)
 	{
 		MyCsharp pin = new MyCsharp();
-		pin.println("Clarisse Andal");
+		pin.println("Andal Clarisse");
 	}
 	
 	public void printName(string name)
