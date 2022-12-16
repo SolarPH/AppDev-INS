2814fafcdb9ab432624fa73c8cf0c42f08d85f46
LaurenRendon <120573046+LaurenRendon@users.noreply.github.com>
2022-12-16 10:34:43 +0800

    Update MyCsharp.cs

--- MyCsharp.cs
+++ MyCsharp.cs
@@ -7,6 +7,10 @@ public class MyCsharp
 		// Insert code from line below this comment where it instantiates the MyCsharp object into variable named pin, then call the method named printName(String name);
     // Expected Line 1, replace this comment with code on pull request
     // Expected Line 2, replace this comment with code on pull request
+		
+		MyJava mjava = new MyJava();
+		pin.printName(" RENDON, JONLAURE ");
+		
 	}
 	
 	public void printName(string name)
