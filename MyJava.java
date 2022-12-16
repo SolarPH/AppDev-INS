d96f9fd8866ff9075e0fc8868b321c4f0e587cd5
LaurenRendon <120573046+LaurenRendon@users.noreply.github.com>
2022-12-16 10:03:22 +0800

    Update MyJava.java

--- MyJava.java
+++ MyJava.java
@@ -5,6 +5,11 @@ public class MyJava
     // Insert code from line below this comment where it instantiates the MyJava object into variable named pin, then call the method named printName(String name);
     // Expected Line 1, replace this comment with code on pull request
     // Expected Line 2, replace this comment with code on pull request
+    
+    MyJava mjava = new Myjava():
+    pin.printName("RENDON, JON LAUREN");
+    
+    
   }
   
   public void printName(String name)
