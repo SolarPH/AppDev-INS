969df111d19872f7d634ee8ad09f12ed94e501a2
MercyPls02 <120582725+MercyPls02@users.noreply.github.com>
2022-12-16 08:10:45 +0800

    Update MyCsharp.cs

--- MyCsharp.cs
+++ MyCsharp.cs
@@ -1,26 +1,24 @@
-using System;
-					
+using System.Runtime.InteropServices;
+
 public class MyCsharp
 {
 
-	
-	public static void Main(string[] args)
 
-	{
-		
-		MyCsharp = new MyCsharp
+    public static void Main(string[] args)
 
-		pin.printName("Mark Zedricx Corpuz");
+    {
+        
+        MyCsharp pin = new MyCsharp();
 
-		// Insert code from line below this comment where it instantiates the MyCsharp object into variable named pin, then call the method named printName(String name);
-    // Expected Line 1, replace this comment with code on pull request
-    // Expected Line 2, replace this comment with code on pull request
-	}
-	
-	public void printName(string name)
+        pin.printName("Mark Zedricx Corpuz");
+        
+    }
 
-	{
-		Console.WriteLine("Hello! My name is " + name);
-	}
+    public void printName(string name)
+
+    {
+        Console.WriteLine("Hello! My name is " + name);
+        Console.ReadKey();
+    }
 
 }
